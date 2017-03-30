3fb1fce32110a5267d2db7678d916558697648eb
ChrisShepherd81 <christian@inf-schaefer.de>
2017-03-30 18:56:18 +0200

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -8,46 +8,21 @@
 #include "ground_truth_package.h"
 #include "measurement_package.h"
 
-using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+
 using std::vector;
+using std::string;
+using std::cerr;
+using std::cout;
+using std::ifstream;
+using std::ofstream;
+using std::endl;
+using std::istringstream;
 
-void check_arguments(int argc, char* argv[]) {
-  string usage_instructions = "Usage instructions: ";
-  usage_instructions += argv[0];
-  usage_instructions += " path/to/input.txt output.txt";
-
-  bool has_valid_args = false;
-
-  // make sure the user has provided input and output files
-  if (argc == 1) {
-    cerr << usage_instructions << endl;
-  } else if (argc == 2) {
-    cerr << "Please include an output file.\n" << usage_instructions << endl;
-  } else if (argc == 3) {
-    has_valid_args = true;
-  } else if (argc > 3) {
-    cerr << "Too many arguments.\n" << usage_instructions << endl;
-  }
-
-  if (!has_valid_args) {
-    exit(EXIT_FAILURE);
-  }
-}
-
-void check_files(ifstream& in_file, string& in_name,
-                 ofstream& out_file, string& out_name) {
-  if (!in_file.is_open()) {
-    cerr << "Cannot open input file: " << in_name << endl;
-    exit(EXIT_FAILURE);
-  }
-
-  if (!out_file.is_open()) {
-    cerr << "Cannot open output file: " << out_name << endl;
-    exit(EXIT_FAILURE);
-  }
-}
+void check_arguments(int argc, char* argv[]);
+void check_files(ifstream& in_file, string& in_name, ofstream& out_file, string& out_name);
+void read_file(ifstream& in_file, vector<MeasurementPackage> &measurement_pack_list, vector<GroundTruthPackage> &gt_pack_list);
 
 int main(int argc, char* argv[]) {
 
@@ -64,65 +39,7 @@ int main(int argc, char* argv[]) {
   vector<MeasurementPackage> measurement_pack_list;
   vector<GroundTruthPackage> gt_pack_list;
 
-  string line;
-
-  // prep the measurement packages (each line represents a measurement at a
-  // timestamp)
-  while (getline(in_file_, line)) {
-
-    string sensor_type;
-    MeasurementPackage meas_package;
-    GroundTruthPackage gt_package;
-    istringstream iss(line);
-    long long timestamp;
-
-    // reads first element from the current line
-    iss >> sensor_type;
-    if (sensor_type.compare("L") == 0) {
-      // LASER MEASUREMENT
-
-      // read measurements at this timestamp
-      meas_package.sensor_type_ = MeasurementPackage::LASER;
-      meas_package.raw_measurements_ = VectorXd(2);
-      float x;
-      float y;
-      iss >> x;
-      iss >> y;
-      meas_package.raw_measurements_ << x, y;
-      iss >> timestamp;
-      meas_package.timestamp_ = timestamp;
-      measurement_pack_list.push_back(meas_package);
-    } else if (sensor_type.compare("R") == 0) {
-      // RADAR MEASUREMENT
-
-      // read measurements at this timestamp
-      meas_package.sensor_type_ = MeasurementPackage::RADAR;
-      meas_package.raw_measurements_ = VectorXd(3);
-      float ro;
-      float phi;
-      float ro_dot;
-      iss >> ro;
-      iss >> phi;
-      iss >> ro_dot;
-      meas_package.raw_measurements_ << ro, phi, ro_dot;
-      iss >> timestamp;
-      meas_package.timestamp_ = timestamp;
-      measurement_pack_list.push_back(meas_package);
-    }
-
-    // read ground truth data to compare later
-    float x_gt;
-    float y_gt;
-    float vx_gt;
-    float vy_gt;
-    iss >> x_gt;
-    iss >> y_gt;
-    iss >> vx_gt;
-    iss >> vy_gt;
-    gt_package.gt_values_ = VectorXd(4);
-    gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
-    gt_pack_list.push_back(gt_package);
-  }
+  read_file(in_file_, measurement_pack_list, gt_pack_list);
 
   // Create a Fusion EKF instance
   FusionEKF fusionEKF;
@@ -181,4 +98,105 @@ int main(int argc, char* argv[]) {
   }
 
   return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void check_arguments(int argc, char* argv[]) {
+  string usage_instructions = "Usage instructions: ";
+  usage_instructions += argv[0];
+  usage_instructions += " path/to/input.txt output.txt";
+
+  bool has_valid_args = false;
+
+  // make sure the user has provided input and output files
+  if (argc == 1) {
+    cerr << usage_instructions << endl;
+  } else if (argc == 2) {
+    cerr << "Please include an output file.\n" << usage_instructions << endl;
+  } else if (argc == 3) {
+    has_valid_args = true;
+  } else if (argc > 3) {
+    cerr << "Too many arguments.\n" << usage_instructions << endl;
+  }
+
+  if (!has_valid_args) {
+    exit(EXIT_FAILURE);
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void check_files(ifstream& in_file, string& in_name,
+                 ofstream& out_file, string& out_name) {
+  if (!in_file.is_open()) {
+    cerr << "Cannot open input file: " << in_name << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  if (!out_file.is_open()) {
+    cerr << "Cannot open output file: " << out_name << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void read_file(ifstream& in_file, vector<MeasurementPackage> &measurement_pack_list, vector<GroundTruthPackage> &gt_pack_list)
+{
+	string line;
+	// prep the measurement packages (each line represents a measurement at a
+	// timestamp)
+	while (getline(in_file, line))
+	{
+		string sensor_type;
+		MeasurementPackage meas_package;
+		GroundTruthPackage gt_package;
+		istringstream iss(line);
+		long long timestamp;
+
+		// reads first element from the current line
+		iss >> sensor_type;
+		if (sensor_type.compare("L") == 0)
+		{
+			// LASER MEASUREMENT
+
+			// read measurements at this timestamp
+			meas_package.sensor_type_ = MeasurementPackage::LASER;
+			meas_package.raw_measurements_ = VectorXd(2);
+			float x;
+			float y;
+			iss >> x;
+			iss >> y;
+			meas_package.raw_measurements_ << x, y;
+			iss >> timestamp;
+			meas_package.timestamp_ = timestamp;
+			measurement_pack_list.push_back(meas_package);
+		}
+		else if (sensor_type.compare("R") == 0)
+		{
+			// RADAR MEASUREMENT
+
+			// read measurements at this timestamp
+			meas_package.sensor_type_ = MeasurementPackage::RADAR;
+			meas_package.raw_measurements_ = VectorXd(3);
+			float ro;
+			float phi;
+			float ro_dot;
+			iss >> ro;
+			iss >> phi;
+			iss >> ro_dot;
+			meas_package.raw_measurements_ << ro, phi, ro_dot;
+			iss >> timestamp;
+			meas_package.timestamp_ = timestamp;
+			measurement_pack_list.push_back(meas_package);
+		}
+
+		// read ground truth data to compare later
+		float x_gt;
+		float y_gt;
+		float vx_gt;
+		float vy_gt;
+		iss >> x_gt;
+		iss >> y_gt;
+		iss >> vx_gt;
+		iss >> vy_gt;
+		gt_package.gt_values_ = VectorXd(4);
+		gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
+		gt_pack_list.push_back(gt_package);
+	}
 }
