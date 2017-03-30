deb70a17d5c6d830d67a0525ba0a8bf22503bbe8
christian <christian@macbook.fritz.box>
2017-03-30 19:34:31 +0200

    Implemented RMSE method.

--- src/tools.cpp
+++ src/tools.cpp
@@ -8,15 +8,37 @@ using std::vector;
 Tools::Tools() {}
 
 Tools::~Tools() {}
-
+///////////////////////////////////////////////////////////////////////////////////////
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
-}
+                              const vector<VectorXd> &ground_truth)
+{
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
 
+	if(estimations.size() != ground_truth.size() || estimations.size() <= 0 )
+	{
+		std::cout << "Error in CalculateRMSE()" << std::endl;
+		return rmse;
+	}
+
+	//accumulate squared residuals
+	for(size_t i=0; i < estimations.size(); ++i)
+	{
+		VectorXd temp = estimations[i] - ground_truth[i];
+		temp = temp.array()*temp.array();
+		rmse += temp;
+	}
+
+	//calculate the mean
+	rmse = rmse/estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
+}
+///////////////////////////////////////////////////////////////////////////////////////
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   /**
   TODO:
