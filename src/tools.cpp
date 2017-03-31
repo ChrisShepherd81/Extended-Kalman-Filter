864d76a001842689c0808d5518a9898a9820cacf
christian <christian@inf-schaefer.de>
2017-03-31 09:00:47 +0200

    Implemented Jacobian Matrix caclulation

--- src/tools.cpp
+++ src/tools.cpp
@@ -39,9 +39,40 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	return rmse;
 }
 ///////////////////////////////////////////////////////////////////////////////////////
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state)
+{
+	MatrixXd Hj(3,4);
+
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	float px2py2 = std::pow(px, 2) + std::pow(py, 2);
+
+	//check division by zero
+	if(px2py2 == 0)
+	{
+		std::cout << "CalculateJacobian() - Error - Division by Zero" << std::endl;
+		return Hj;
+	}
+
+	float sqr_px2py2 = std::sqrt(px2py2);
+	float sqr_px2py2_3 = std::sqrt(std::pow(px2py2, 3));
+
+	//compute the Jacobian matrix
+	Hj(0,0) = px/sqr_px2py2;
+	Hj(0,1) = py/sqr_px2py2;
+
+	Hj(1,0) = -py/px2py2;
+	Hj(1,1) = px/px2py2;
+
+	Hj(2,0) = (py*(vx*py-vy*px))/sqr_px2py2_3;
+	Hj(2,1) = (px*(vy*px-vx*py))/sqr_px2py2_3;
+	Hj(2,2) = px/sqr_px2py2;
+	Hj(2,3) = py/sqr_px2py2;
+
+	return Hj;
 }
+///////////////////////////////////////////////////////////////////////////////////////
